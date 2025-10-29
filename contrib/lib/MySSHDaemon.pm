eeb7a29ba83b65a485bf6321042116198be7d0d3
Rob Brown <bbb@cpan.org>
2025-10-29 03:50:00 -0600

    Add stamp $tag to track hint text.

--- contrib/lib/MySSHDaemon.pm
+++ contrib/lib/MySSHDaemon.pm
@@ -5,21 +5,28 @@ use base qw(Net::SSH::Server);
 
 sub stamp {
     my $self = shift;
+    my $tag = shift || "unknown";
     my $now = do {require Time::HiRes;local$_=[Time::HiRes::gettimeofday()];unshift@$_,localtime$_->[0];sprintf"%04d-%02d-%02d_%02d:%02d:%02d.%06d",$_->[5]+1900,$_->[4]+1,@$_[3,2,1,0,10]};
-    my $ppid = getppid; my @run = @{ $self->{run} };
-    system "echo $now [$$] [$ppid] [@run] ENV: \$(env|sort) >> /tmp/sshclient.log"; chmod 0666, "/tmp/sshclient.log";
+    my $ppid = getppid;
+    my @run = @{ $self->{run} };
+    system "echo $now [$$] [$ppid] [$tag] [@run] ENV: \$(env|sort) >> /tmp/sshclient.log"; chmod 0666, "/tmp/sshclient.log";
+}
+
+sub init {
+    my $self = shift;
+    $self->stamp("init");
 }
 
 sub run_sshd {
     my $self = shift;
     $self->stash->{override_config_file} = "/etc/ssh/sshdproxy_config";
-    $self->stamp;
+    $self->stamp("run_sshd");
     $self->SUPER::run_sshd();
 }
 
 sub run_pam_exec {
     my $self = shift;
-    $self->stamp;
+    $self->stamp("run_pam_exec");
     $self->SUPER::run_pam_exec();
 }
 
